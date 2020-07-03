eded778fe601567aadfeba077fdd7b9b468ca1ce
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-03 13:33:51 -0700

    this will define my user input variables to be used later

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import {CurrencyService} from './exchange-service.js';
 $(document).ready(function () {
   $("#currency-exchange").submit(function(event)  {
     event.preventDefault();
-  })
+    let exchangeAmount = parseInt($("money-amount").val());
+    let countryConvertTo = $("conversion-select").val();
+
+  });
 
   // $('#submit-button').submit(function (event) {
   //     event.preventDefault();
