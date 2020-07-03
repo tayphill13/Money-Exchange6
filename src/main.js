93ada4c2c25bb29b41d2280084e13819bc9e8980
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-03 13:57:45 -0700

    begin my IIFE function in main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {CurrencyService} from './exchange-service.js';
+import {CurrencyExchange} from './exchange-service.js';
 
 $(document).ready(function () {
   $("#currency-exchange").submit(function(event)  {
     event.preventDefault();
     let exchangeAmount = parseInt($("money-amount").val());
     let countryConvertTo = $("conversion-select").val();
-
+    
+    (async () => {
+      let moneyExchange= new CurrencyExchange();
+      let apiResponse = await moneyExchange.getCurrencyConversion();
+    });
   });
 
   // $('#submit-button').submit(function (event) {
