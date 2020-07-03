40e88f9ffdb24c7374c6cfe484108d6c9ad63901
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-03 16:45:24 -0700

    remove my reminder  comments to self in my main.js

--- src/main.js
+++ src/main.js
@@ -7,20 +7,20 @@ import {CurrencyExchange} from './exchange-service.js';
 $(document).ready(function () {
   $("#currency-exchange").submit(function(event)  {
     event.preventDefault();
-    let exchangeAmount = parseInt($("#money-amount").val());  // Use in the makeConversion function to multiply by the conversion rate
+    let exchangeAmount = parseInt($("#money-amount").val());
     let countryConvertTo = $("#conversion-select").val();
     
     (async () => {
-      let moneyExchange= new CurrencyExchange();
+      let moneyExchange = new CurrencyExchange();
       let response = await moneyExchange.getCurrencyConversion();
       makeConversion(response, countryConvertTo);
     })();
 
-    function makeConversion(response, countryConvertTo)  {  // apiResponse is the conversion rate from API call and countryConvert is the selected country for conversion
+    function makeConversion(response, countryConvertTo)  { 
       if (!response && countryConvertTo === null)  {
         $("#result").text("There was an error, it's possible that country is not supported by this application");
       } else if (response && countryConvertTo === "swiss")  {
-        $("#result").text(`${response.conversion_rates.CHF}`*exchangeAmount + ' Swiss Francs'); // Should be the (conversion rate from API) X (money amount entered by user)
+        $("#result").text(`${response.conversion_rates.CHF}`*exchangeAmount + ' Swiss Francs');
       } else if (response && countryConvertTo === "mexican")  {
         $("#result").text(`${response.conversion_rates.MXN}`*exchangeAmount + ' Mexican Pesos');
       } else if (response && countryConvertTo === "canadian") {
