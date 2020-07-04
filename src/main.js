b228f896cf5ed29f73a7347579988205d1ba0e62
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-03 21:17:06 -0700

    edit main.js for spacing and consistency

--- src/main.js
+++ src/main.js
@@ -20,15 +20,15 @@ $(document).ready(function () {
       if (!response && countryConvertTo === null)  {
         $("#result").text("There was an error, it's possible that country is not supported by this application");
       } else if (response && countryConvertTo === "swiss")  {
-        $("#result").text(`${response.conversion_rates.CHF}`*exchangeAmount + ' Swiss Francs');
+        $("#result").text(`${response.conversion_rates.CHF}`* exchangeAmount + ' Swiss Francs');
       } else if (response && countryConvertTo === "mexican")  {
-        $("#result").text(`${response.conversion_rates.MXN}`*exchangeAmount + ' Mexican Pesos');
+        $("#result").text(`${response.conversion_rates.MXN}`* exchangeAmount + ' Mexican Pesos');
       } else if (response && countryConvertTo === "canadian") {
-        $("#result").text(`${response.conversion_rates.CAD}`*exchangeAmount + ' Canadian Dollars');
+        $("#result").text(`${response.conversion_rates.CAD}`* exchangeAmount + ' Canadian Dollars');
       } else if (response && countryConvertTo === "peruvian") {
-        $("#result").text(`${response.conversion_rates.PEN}`*exchangeAmount + ' Peruvian Sol');
+        $("#result").text(`${response.conversion_rates.PEN}`* exchangeAmount + ' Peruvian Sol');
       } else if (response && countryConvertTo === "taiwanese")  {
-        $("#result").text(`${response.conversion_rates.TWD}`*exchangeAmount + ' New Taiwan Dollars');
+        $("#result").text(`${response.conversion_rates.TWD}`* exchangeAmount + ' New Taiwan Dollars');
       } else if (response && countryConvertTo === "uk") {
         $("#result").text(`${response.conversion_rates.GBP}` * exchangeAmount + ' Pounds Stirling'); 
       } else if (response && countryConvertTo === "euros") {
